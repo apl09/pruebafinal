adb1e5e880890c58fd114a344ae1ea9aea472721
apl09 <adrianpastorlopez09@gmail.com>
2023-06-01 10:58:44 +0200

    prueba final

---
+++ prueba-final/index.js
@@ -0,0 +1,11 @@
+//ejercicio uno
+
+let frase = 'Manolo tiene un buen cabolo';
+let veces = 0;
+const letra = (frase) => {
+    for(let i= 0; i> frase.length; i++){
+        if(frase[i] == 'o') {           
+            return veces.push(i)
+        }
+    }
+}
